7690596a4ad64b6ce19026a662ad07b890793b84
ivaniykvictor <ivaniykvictor@gmail.com>
2020-11-09 20:57:56 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const { PORT, accessToken, chatId } = process.env;
+
+const requestListener = async (req, res) => {
+    res.writeHead(200);
+  
+    const parsedUrl = url.parse(req.url, true);
+  
+    if (parsedUrl.pathname === '/') {
+      fs.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
+        if (err) {
+          console.error(err);
+          return res.end(JSON.stringify({ ok: false, description: err.message }));
+        }
+        res.write(text);
+        res.end();
+      });
+    } else if (parsedUrl.pathname === '/send') {
+      const { text } = parsedUrl.query;
+  
+      let tgRes;
+      try {
+        tgRes = await fetch(`https://api.telegram.org/bot${'1283516768:AAHFFHTdc2bOsk0iM6ffemPmqOyCPO4VaCI'}/sendMessage?chat_id=${'-1001167248578'}&text=${text}`);
+        tgRes = await tgRes.json();
+        if (tgRes && tgRes.ok) {
+          return res.end(JSON.stringify({ ok: true }));
+        }
+        throw new Error('No response from telegram');
+      } catch (err) {
+        console.error(err);
+        return res.end(JSON.stringify({ ok: false, description: err.message }));
+      }
+    } else {
+      res.end();
+    }
+  };
+  
+  const server = http.createServer(requestListener);
+  
+  console.log('server starting...');
+  server.listen(PORT, err => {
+    if(err) console.error(err);
+    else console.log('server is up');
+  });
