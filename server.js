86d79faf460e6e37214d6af5b764b99e1abe2ce0
ivaniykvictor <ivaniykvictor@gmail.com>
2020-11-22 16:42:46 +0200

    Modified servis.js

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const fetch = require('node-fetch');
+const path = require('path');
+
 
 const { PORT, accessToken, chatId } = process.env;
+
 
 const requestListener = async (req, res) => {
     res.writeHead(200);
   
     const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === '/.css') {
+      fs.readFile(__dirname + '/css/main.css', (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.end(JSON.stringify({ ok: false, description: err.message }));
+        }
+        // res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        // res.end(content, 'utf-8');
+        // res.end();
+        res.write(data);
+        res.end();
+      }); 
+      
+    };
   
     if (parsedUrl.pathname === '/') {
-      fs.readFile(__dirname + '/index.html', __dirname + '/main.css', 'utf8', (err, text) => {
+      fs.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
         if (err) {
           console.error(err);
           return res.end(JSON.stringify({ ok: false, description: err.message }));
@@ -19,6 +37,7 @@ const requestListener = async (req, res) => {
         res.write(text);
         res.end();
       });
+      
     } else if (parsedUrl.pathname === '/send') {
       const { text } = parsedUrl.query;
   
@@ -46,3 +65,58 @@ const requestListener = async (req, res) => {
     if(err) console.error(err);
     else console.log('server is up');
   });
+
+// var http = require('http')
+// , url = require('url')
+// , fs = require('fs')
+// , path = require('path')
+// , server;
+
+
+
+//   const server = http.createServer(function(req,res){
+
+//       var myPath = url.parse(req.url).pathname;
+    
+//         switch(myPath){
+    
+//           case '/FREESTYLE':
+    
+//             // get the extensions of the files inside this dir (.html, .js, .css)
+//             var extname = mypath.extname(path);
+    
+//               switch (extname) {
+    
+//                 //get the html
+//                 case '.html':
+//                   fs.readFile(__dirname + '/FREESTYLE/index.html', function (err, data) {
+//                     if (err) return send404(res);
+//                     res.writeHead(200, {'Content-Type': 'text/html'});
+//                     res.write(data, 'utf8');
+//                     res.end();
+//                   });
+//                 break;
+//                  //get the script that /public/chatclient.html references
+//             case '.js':
+//               fs.readFile(__dirname + '/FREESTYLE/js/main.js', function (err, data) {
+//                 if (err) return send404(res);
+//                 res.writeHead(200, { 'Content-Type': 'text/javascript' });
+//                 res.end(content, 'utf-8');
+//                 res.end();
+//               });
+//             break;
+
+//             // get the styles that /public/chatclient.html references
+//             case '.css':
+//               fs.readFile(__dirname + '/FREESTYLE/css/main.css', function (err, data) {
+//                 if (err) return send404(res);
+//                 res.writeHead(200, { 'Content-Type': 'text/javascript' });
+//                 res.end(content, 'utf-8');
+//                 res.end();
+//               });
+//           }
+//           break;
+
+//           default: send404(res);
+//         }
+//     });
